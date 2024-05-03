325125c2b182f4af314845b9609534018686ce6a
wijdenchaabouni <134175868+wijdenchaabouni@users.noreply.github.com>
2024-05-03 19:18:22 +0200

    Add files via upload

---
+++ rooms-quickstart-java/README.md
@@ -0,0 +1,38 @@
+---
+page_type: sample
+languages:
+- java
+products:
+- azure
+- azure-communication-services
+---
+
+# Azure Communication Services - Rooms
+
+This quickstart will help you get started with Azure Communication Services Rooms. A `room` is a server-managed communications space for a known, fixed set of participants to collaborate for a pre-determined duration. The [rooms conceptual documentation](https://docs.microsoft.com/azure/communication-services/concepts/rooms/room-concept) covers more details and use cases for `rooms`.
+
+## Prerequisites
+
+- An Azure account with an active subscription. [Create an account for free](https://azure.microsoft.com/free/?WT.mc_id=A261C142F).
+- An active Communication Services resource and connection string.
+- Two or more Communication User Identities.
+- [Java Development Kit (JDK)](/java/azure/jdk/?view=azure-java-stable&preserve-view=true) version 8 or above.
+- [Apache Maven](https://maven.apache.org/download.cgi)
+
+## Code structure
+
+- **src/main/java/com/communication/rooms/quickstart/App.java**: includes source code for manging rooms and room participants.
+- **pom.xml:** Project's Project Object Model, or [POM](https://maven.apache.org/guides/introduction/introduction-to-the-pom.html).
+
+## Before running sample code
+
+1. Open an instance of PowerShell, Windows Terminal, Command Prompt or equivalent and navigate to the directory that you'd like to clone the sample to.
+1. `git clone https://github.com/Azure-Samples/communication-services-java-quickstarts.git`
+1. With the Communication Services resource connection string procured in pre-requisites, replace the placeholder at line no 31
+    ```String connectionString = "<connection-string>";```.
+
+## Run the code
+
+1. Navigate to the directory containing the pom.xml file and compile the project by using command `mvn compile`.
+2. Build the package using command `mvn package`.
+3. Run the command to execute the app `mvn -e exec:java -D"exec.mainClass"="com.communication.rooms.quickstart.App" -D"exec.cleanupDaemonThreads"=false`.
---
+++ rooms-quickstart-java/src/main/java/com/communication/rooms/quickstart/App.java
@@ -0,0 +1,322 @@
+package com.communication.rooms.quickstart;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.azure.communication.rooms.models.AddOrUpdateParticipantsResult;
+import com.azure.communication.rooms.models.CommunicationRoom;
+import com.azure.communication.rooms.models.CreateRoomOptions;
+import com.azure.communication.rooms.models.ParticipantRole;
+import com.azure.communication.rooms.models.RemoveParticipantsResult;
+import com.azure.communication.rooms.models.RoomParticipant;
+import com.azure.communication.rooms.models.UpdateRoomOptions;
+import com.azure.communication.rooms.implementation.models.CommunicationErrorResponseException;
+import com.azure.communication.common.CommunicationIdentifier;
+import com.azure.communication.common.implementation.CommunicationConnectionString;
+import com.azure.communication.rooms.RoomsClient;
+import com.azure.communication.rooms.RoomsClientBuilder;
+import com.azure.communication.identity.CommunicationIdentityClient;
+import com.azure.communication.identity.CommunicationIdentityClientBuilder;
+import com.azure.core.http.rest.PagedIterable;
+import com.azure.core.http.rest.PagedResponse;
+import com.azure.core.util.Context;
+import com.azure.core.credential.AzureKeyCredential;
+
+public class App {
+    static String CONNECTION_STRING = "<connection-string>";
+    static RoomParticipant participant_1;
+    static RoomParticipant participant_2;
+    static RoomParticipant participant_3;
+
+    static RoomsClient roomsClient;
+    static CommunicationIdentityClient communicationClient;
+
+    public static RoomsClient createRoomsClientWithConnectionString() {
+        RoomsClient roomsClient = new RoomsClientBuilder().connectionString(CONNECTION_STRING).buildClient();
+
+        return roomsClient;
+    }
+
+    public static CommunicationIdentityClientBuilder getCommunicationIdentityClientBuilder() {
+        CommunicationIdentityClientBuilder builder = new CommunicationIdentityClientBuilder();
+        CommunicationConnectionString connectionStringObject = new CommunicationConnectionString(CONNECTION_STRING);
+        String endpoint = connectionStringObject.getEndpoint();
+        String accessKey = connectionStringObject.getAccessKey();
+        builder.endpoint(endpoint)
+                .credential(new AzureKeyCredential(accessKey));
+
+        return builder;
+    }
+
+    public static CommunicationRoom createRoom() {
+        OffsetDateTime validFrom = OffsetDateTime.now();
+        OffsetDateTime validUntil = validFrom.plusDays(30);
+
+        List<RoomParticipant> roomParticipants = new ArrayList<RoomParticipant>();
+
+        roomParticipants.add(participant_1);
+        roomParticipants.add(participant_2.setRole(ParticipantRole.CONSUMER));
+
+        System.out.print("Creating room...\n");
+
+        CreateRoomOptions roomOptions = new CreateRoomOptions()
+                .setValidFrom(validFrom)
+                .setValidUntil(validUntil)
+                .setPstnDialOutEnabled(false)
+                .setParticipants(roomParticipants);
+
+        return roomsClient.createRoom(roomOptions);
+
+    }
+
+    public static boolean deleteRoom(String roomId) {
+        try {
+            System.out.println(roomId);
+            System.out.print("Removing room...\n");
+            roomsClient.deleteRoomWithResponse(roomId, Context.NONE);
+        } catch (CommunicationErrorResponseException ex) {
+            if (ex.getResponse().getStatusCode() == 404) {
+                System.out.println("Room already deleted");
+                return false;
+            }
+        }
+        System.out.println("Deleted");
+        return true;
+    }
+
+    public static void getRoom(String roomId) {
+        try {
+            System.out.print("Getting room...\n");
+
+            CommunicationRoom roomResult = roomsClient.getRoom(roomId);
+            System.out.println("RoomId: " + roomResult.getRoomId());
+            System.out.println("Created at: " + roomResult.getCreatedAt());
+
+            System.out.println("ValidFrom: " + roomResult.getValidFrom());
+            System.out.println("ValidUntil: " + roomResult.getValidUntil());
+            System.out.println("PstnDialOutEnabled: " + roomResult.isPstnDialOutEnabled());
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+    }
+
+    public static void updateRoom(String roomId) {
+
+        try {
+            OffsetDateTime validFrom = OffsetDateTime.now().plusDays(1);
+            OffsetDateTime validUntil = validFrom.plusDays(1);
+
+            UpdateRoomOptions roomUpdateOptions = new UpdateRoomOptions()
+                    .setValidFrom(validFrom)
+                    .setValidUntil(validUntil)
+                    .setPstnDialOutEnabled(true);
+
+            System.out.print("Updating room...\n");
+
+            CommunicationRoom roomResult = roomsClient.updateRoom(roomId, roomUpdateOptions);
+            
+            System.out.println("RoomId: " + roomResult.getRoomId());
+            System.out.println("Created at: " + roomResult.getCreatedAt());
+            System.out.println("ValidFrom: " + roomResult.getValidFrom());
+            System.out.println("ValidUntil: " + roomResult.getValidUntil());
+            System.out.println("PstnDialOutEnabled: " + roomResult.isPstnDialOutEnabled());
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public static void listRooms() {
+        try {
+            Iterable<PagedResponse<CommunicationRoom>> roomPages = roomsClient.listRooms().iterableByPage();
+
+            System.out.println("Listing all the rooms IDs in the first two pages of the list of rooms:");
+
+            int count = 0;
+            for (PagedResponse<CommunicationRoom> page : roomPages) {
+                for (CommunicationRoom room : page.getElements()) {
+                    System.out.println("\n" + room.getRoomId());
+                }
+
+                count++;
+                if (count >= 2) {
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public static void addOrUpdateParticipants(String roomId) {
+        try {
+            List<RoomParticipant> participantsToAddAndUpdate = new ArrayList<>();
+
+            // Adding new participant
+            participantsToAddAndUpdate.add(participant_3.setRole(ParticipantRole.CONSUMER));
+
+            // Updating current participant
+            participantsToAddAndUpdate.add(participant_2.setRole(ParticipantRole.PRESENTER));
+
+            System.out.print("Adding/Updating participants())...\n");
+
+            AddOrUpdateParticipantsResult addOrUpdateParticipantsResult = roomsClient.addOrUpdateParticipants(roomId,
+                    participantsToAddAndUpdate);
+
+            System.out.println("Participant(s) added/updated");
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public static void removeParticipant(String roomId) {
+        try {
+            List<CommunicationIdentifier> participantsToRemove = new ArrayList<>();
+
+            participantsToRemove.add(participant_3.getCommunicationIdentifier());
+
+            System.out.print("Removing participant(s)...\n");
+
+            RemoveParticipantsResult removeParticipantsResult = roomsClient.removeParticipants(roomId,
+                    participantsToRemove);
+
+            System.out.println("Participant(s) removed");
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public static void listParticipants(String roomId) {
+        try {
+            System.out.print("Listing participant(s)...\n");
+
+            PagedIterable<RoomParticipant> participants = roomsClient.listParticipants(roomId);
+
+            for (RoomParticipant participant : participants) {
+                System.out.println(
+                        participant.getCommunicationIdentifier().getRawId() + " (" + participant.getRole() + ")");
+            }
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public static void main(String[] args) {
+        roomsClient = createRoomsClientWithConnectionString();
+        communicationClient = getCommunicationIdentityClientBuilder().buildClient();
+        participant_1 = new RoomParticipant(communicationClient.createUser());
+        participant_2 = new RoomParticipant(communicationClient.createUser());
+        participant_3 = new RoomParticipant(communicationClient.createUser());
+
+        int selection;
+        Set<String> roomIds = new HashSet<>();
+
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
+            while (true) {
+
+                System.out.println("Make a selection");
+                System.out.println("1. Add a room");
+                System.out.println("2. Update a room by extending validUntil by one day");
+                System.out.println("3. Delete a room");
+                System.out.println("4. Get room details");
+                System.out.println("5. List room ids created in this session");
+                System.out.println("6. List rooms in resource");
+                System.out.println("7. Add or Update random participant");
+                System.out.println("8. Remove a random participant");
+                System.out.println("9. List participants");
+                System.out.println("10. Exit");
+                selection = Integer.parseInt(br.readLine());
+                switch (selection) {
+                    case 1:
+                        CommunicationRoom roomResult = createRoom();
+                        getRoom(roomResult.getRoomId());
+                        roomIds.add(roomResult.getRoomId());
+                        break;
+                    case 2: {
+                        System.out.print("RoomId:");
+                        String roomId = br.readLine();
+
+                        updateRoom(roomId);
+                        break;
+                    }
+                    case 3: {
+                        System.out.print("RoomId:");
+                        String roomId = br.readLine();
+
+                        if (deleteRoom(roomId)) {
+                            roomIds.remove(roomId);
+                        }
+                        break;
+                    }
+                    case 4: {
+                        System.out.print("RoomId:");
+                        String roomId = br.readLine();
+
+                        getRoom(roomId);
+
+                        break;
+                    }
+                    case 5: {
+                        System.out.print("Listing room ids...\n");
+                        for (String room : roomIds) {
+                            System.out.println(room);
+                        }
+                        break;
+                    }
+                    case 6: {
+                        listRooms();
+
+                        break;
+                    }
+                    case 7: {
+                        System.out.print("RoomId:");
+                        String roomId = br.readLine();
+
+                        addOrUpdateParticipants(roomId);
+
+                        break;
+                    }
+                    case 8: {
+                        System.out.print("RoomId:");
+                        String roomId = br.readLine();
+
+                        removeParticipant(roomId);
+
+                        break;
+                    }
+                    case 9: {
+                        System.out.print("RoomId:");
+                        String roomId = br.readLine();
+
+                        listParticipants(roomId);
+
+                        break;
+                    }
+                    case 10:
+                        System.out.println("Deleting all rooms and exiting program...");
+                        for (String room : roomIds) {
+                            System.out.println("Deleting:" + room);
+                            deleteRoom(room);
+                        }
+
+                        System.exit(0);
+
+                        return;
+                    default:
+                        break;
+                }
+
+            }
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+        }
+    }
+}
